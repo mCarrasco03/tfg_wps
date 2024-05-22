fe93f988cadcaf1637436e7764baad4db3c7c837
mCarrasco03 <127890760+mCarrasco03@users.noreply.github.com>
2024-05-22 23:26:55 +0200

    Add files via upload

---
+++ infoContacto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Información Instalaciones</title>
+    <link rel="stylesheet" href="css/infoContacto.css" type="text/css">
+</head>
+<body>
+<?php include "includes/header_repag.php"?>
+<section>
+    <div id="contenedor_img">
+    <div>
+    <img src="img/instalaciones3.jpg">
+    </div>
+    <div id="contenedor_ubicacion">
+        <h3>Imagen tomada de las instalaciones del club. Estamos ubicados <a href="#">aquí</a></h3>
+    </div>
+    </div>
+    <div id="contenedor_info">
+        <div id="contenedor_titulo">
+            <h2>World Padel Club</h2>
+            <p>Organiza tus partidos rápidamente</p>
+        </div>
+        <div id="contenedor_texto">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint inventore fugit eveniet ipsam necessitatibus debitis quia soluta consectetur quam, impedit eos reiciendis consequatur dignissimos, similique ipsum fugiat amet accusamus eius?</p>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio recusandae blanditiis voluptates provident, unde quibusdam vitae similique inventore molestiae tempore incidunt accusamus ipsam adipisci aliquid rerum quisquam doloribus expedita perferendis.</p>      
+        </div>
+        <div id="contenedor_contacto">
+        <button>Contactar</button>
+        </div>
+
+    </div>
+</section>
+</body>
+</html>
